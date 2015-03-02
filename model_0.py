28109bde6912db72436ed58ea67ac152636a2497
Steven Chu <stevenchu@142-254-111-216.dsl.dynamic.fusionbroadband.com>
2015-03-01 16:32:08 -0800

    second commit of model

--- model_0.py
+++ model_0.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 
 from sklearn import tree
 from sklearn import linear_model
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Imputer
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -18,29 +17,34 @@ testData = pd.read_csv("data/test.csv")
 users = pd.read_csv('data/users_pretty_dummy.csv')
 
 
+# merging data to have more things together
 newTestData = pd.merge(testData, users, left_on='User', right_on='RESPID', how='left')
 newTrainData = pd.merge(trainData, users, left_on='User', right_on='RESPID', how='left')
 
+
+# investigating resulting data to make sure columns, etc. line up
 # print len(newTestData.columns)
 # print newTestData.columns, '\n'
 
 # print len(newTrainData.columns)
 # print newTrainData.columns, '\n'
 
+
+# take a random sample of this testing data
 n = range(len(newTestData))
 np.random.seed(0)
 sampleIndex = np.random.choice(n, 10000, replace=False)
-
 sampleData = newTestData.loc[sampleIndex]
 sampleData.reset_index()
 
-# print sampleData.head()
 
-sampleData.fillna(0, inplace=True)
+# replacing nan's with 0, going to try something different for this point
+# sampleData.fillna(sampleData.median(), inplace=True)
+# sampleData.fillna(sampleData.mode(), inplace=True)
+sampleData.fillna(sampleData.mean(), inplace=True)
 
-# print sampleData.head()
 
-
+# iterating through each testing data point, making predictions
 for index, row in sampleData.iterrows():
     artist = row['Artist']
     track = row['Track']
